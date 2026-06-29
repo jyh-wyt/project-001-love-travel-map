951b35f8c2b8c63b320dc8dc8d3aaa6d40cad587
jyh-wyt <2278901840@qq.com>
2026-06-29 21:28:05 +0800

    change

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -6,7 +6,15 @@ import "./styles.css";
 
 export const metadata: Metadata = {
   title: "\u60c5\u4fa3\u65c5\u884c\u5730\u56fe",
-  description: "\u7ed9\u60c5\u4fa3\u4f7f\u7528\u7684\u79c1\u5bc6\u65c5\u884c\u8bb0\u5f55\u4e0e\u65c5\u884c\u8ba1\u5212\u7f51\u9875"
+  description: "\u7ed9\u60c5\u4fa3\u4f7f\u7528\u7684\u79c1\u5bc6\u65c5\u884c\u8bb0\u5f55\u4e0e\u65c5\u884c\u8ba1\u5212\u7f51\u9875",
+  icons: {
+    icon: [
+      { url: "/love-travel-icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/love-travel-icon.png", sizes: "512x512", type: "image/png" }
+    ],
+    shortcut: ["/love-travel-icon-192.png"],
+    apple: [{ url: "/love-travel-icon.png", sizes: "512x512", type: "image/png" }]
+  }
 };
 
 const themeInitScript = `
